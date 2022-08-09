b5458981aabfd9621f40ccaa91074381eac659a1
Pedro <pedrop6@hotmail.com.br>
2022-08-09 16:26:52 -0700

    Small Changes

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,21 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Welcome to my awesome GameEngine" << std::endl;
+	std::cout << "Welcome to my awesome GameEngine" << '\n';
 
+	std::cout << "Initializing Game..." << '\n';
 	Game *game = new Game();
 	game->Initialize(WINDOW_WIDTH, WINDOW_HEIGHT, false);
+	std::cout << "Game initialized" << '\n';
 
+	std::cout << "Starting Game Loop..." << '\n';
 	while (Game::IsRunning()) {
 		game->Update();
 	}
 
+	std::cout << "Game Loop ended" << '\n';
 	game->Destroy();
+	std::cout << "Game destroyed" << std::endl;
+
+	return 0;
 }
